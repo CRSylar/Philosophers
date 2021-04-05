a3c39bbcca4c9a3c32cb24cff3ab6930e1707035
CRSylar <cristianoromaldetti@icloud.com>
2021-04-05 16:17:34 +0200

    philo_3 start, da testare + normare + leakkare

---
+++ includes/philo_three.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_three.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cromalde <cromalde@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/05 11:30:18 by cromalde          #+#    #+#             */
+/*   Updated: 2021/04/05 15:49:18 by cromalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_THREE_H
+# define PHILO_THREE_H
+
+# include <stdlib.h>
+# include <errno.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <semaphore.h>
+# include <fcntl.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+struct	s_all;
+
+typedef struct s_philo
+{
+	pid_t			*pid;
+	int					id;
+	int					m_start;
+	int					m_end;
+	unsigned long		t_last_meal;
+	unsigned long		t_to_live;
+	pthread_t			t;
+	sem_t				*eating;
+	struct s_all		*all;
+}				t_philo;
+
+typedef struct s_all
+{
+	t_philo				p;
+	int					status;
+	int					philo;
+	int					sleep;
+	int					eat;
+	int					die;
+	int					is_dead;
+	int					n_meal;
+	int					total_meal;
+	unsigned long		time_start;
+	sem_t				*forks;
+	sem_t				*print;
+	sem_t				*dead;
+}					t_all;
+
+int				ft_atoi(const char *nptr);
+unsigned long	now(void);
+void			*life_status(void *ptr);
+void			ft_print(t_philo *p, char *str);
+void			go_to_sleep(int time, t_all *all);
+
+#endif
--- ph_one_srcs/ph_three.c
+++ ph_one_srcs/ph_three.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ph_two.c                                           :+:      :+:    :+:   */
+/*   ph_three.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cromalde <cromalde@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 11:30:11 by cromalde          #+#    #+#             */
-/*   Updated: 2021/04/05 15:44:54 by cromalde         ###   ########.fr       */
+/*   Updated: 2021/04/05 16:13:18 by cromalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/philo_two.h"
+#include "../includes/philo_three.h"
 
 int	ft_check_imput(int ac, char **av, t_all *all)
 {
@@ -27,12 +27,14 @@ int	ft_check_imput(int ac, char **av, t_all *all)
 	if ((all->philo < 1) || (all->die < 0) || (all->eat < 0) || \
 		(all->sleep < 0) || (ac == 6 && all->n_meal < 0))
 		return (printf("One or More Params is Wrong\n"));
-	all->p = malloc(sizeof(t_philo) * all->philo);
-	if (!all->p)
+	all->p.pid = malloc(sizeof(pid_t) * all->philo);
+	if (!all->p.pid)
 		return (printf("Error: memory not allocated\n"));
 	sem_unlink("print");
 	sem_unlink("dead");
 	sem_unlink("forks");
+	sem_unlink("eating");
+	all->print = sem_open("eating", O_CREAT | O_EXCL, 0777, 1);
 	all->print = sem_open("print", O_CREAT | O_EXCL, 0777, 1);
 	all->dead = sem_open("dead", O_CREAT | O_EXCL, 0777, 1);
 	all->forks = sem_open("forks", O_CREAT | O_EXCL, 0777, all->philo);
@@ -48,9 +50,7 @@ void	go_eat(t_philo *p)
 	sem_wait(p->eating);
 	p->t_last_meal = now();
 	ft_print(p, "is eating");
-	p->dop_start++;
-	if (p->all->total_meal > 0)
-		p->all->total_meal -= 1;
+	p->m_start++;
 	go_to_sleep(p->all->eat, p->all);
 	sem_post(p->eating);
 	sem_post(p->all->forks);
@@ -68,63 +68,63 @@ void	*cicle(void *ptr)
 	p = (t_philo *)ptr;
 	p->t_last_meal = now();
 	if (pthread_create(&check, 0, life_status, p) != 0)
-		return (0);
+		exit(1);
 	pthread_detach(check);
 	if (p->id % 2 == 0)
 		go_to_sleep(p->all->eat * 0.9, p->all);
-	while (!p->all->is_dead && (p->dop_end == -1 || p->dop_start < p->dop_end))
+	while (!p->all->is_dead && (p->m_end == -1 || p->m_start < p->m_end))
 		go_eat(p);
+	if (!p->all->is_dead && p->m_end != -1 && p->m_start == p->m_end)
+		exit(2);
 	return (0);
 }
 
-void	ft_start_loop(t_all *all, int i, int j, int k)
+int	ft_start_loop(t_all *all, int i, int j, int check)
 {
-	void	*philo;
-
+	all->status = 0;
+	all->p.m_start = 0;
+	if (all->n_meal > 0)
+		all->p.m_end = all->n_meal;
 	while ((++i) < all->philo)
 	{
-		all->p[i].all = all;
-		all->p[i].eating = sem_open("eating", O_CREAT | O_EXCL, 0777, 1);
-		all->p[i].id = i + 1;
-		all->p[i].eat = 0;
-		if (all->n_meal >= 0)
-			all->p[i].dop_end = all->n_meal;
-		else
-			all->p[i].dop_end = -1;
-		all->p[i].dop_start = 0;
+		all->p.pid[i] = fork();
+		if (all->p.pid[i] < 0)
+			return (printf("Cannot fork!\n"));
+		else if (all->p.pid[i] == 0)
+		{
+			all->p.id = i + 1;
+			cicle((void *)(&all->p));
+		}
+	}
+	while (1)
+	{
+		waitpid(-1, &all->status, 0);
+		if (all->status == 1)
+			break ;
+		else if ((all->status == 2) && (++check == all->philo))
+			break ;
 	}
 	while ((++j) < all->philo)
-	{
-		philo = (void *)(&all->p[j]);
-		if (pthread_create(&all->p[j].t, 0, cicle, philo) != 0)
-			return ;
-	}
-	while ((++k) < all->philo)
-		pthread_join(all->p[k].t, 0);
-	i = -1;
-	while ((++i) < all->philo)
-		k = sem_close(all->p[i].eating) + sem_unlink("eating");
+		kill(all->p.pid[j], SIGTERM);
 }
 
 int	main(int ac, char **av)
 {
 	t_all	all;
 
+	all.p.all = &all;
 	if (ft_check_imput(ac, av, &all))
 		return (1);
 	all.is_dead = 0;
-	if (ac == 6 && all.n_meal > 0)
-		all.total_meal = all.n_meal * all.philo;
-	else
-		all.total_meal = -1;
 	all.time_start = now();
-	ft_start_loop(&all, -1, -1, -1);
+	ft_start_loop(&all, -1, -1, 0);
 	sem_close(all.dead);
 	sem_close(all.print);
 	sem_close(all.forks);
+	sem_close(all.p.eating);
+	sem_unlink("eating");
 	sem_unlink("print");
 	sem_unlink("forks");
 	sem_unlink("dead");
-	free(all.p);
 	return (0);
 }
